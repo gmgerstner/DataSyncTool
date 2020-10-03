44d87d7c1531a9b71fa8f141338504e3b22ade22
George M. Gerstner <gmgerstner@gmail.com>
2020-10-02 19:30:06 -0500

    some code in ui

--- GMG.DataSyncTool.WpfUI/MainWindow.xaml.cs
+++ GMG.DataSyncTool.WpfUI/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace GMG.DataSyncTool.WpfUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string SourceConnectionString { get; set; }
+        private string TargetConnectionString { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectSourceDatabaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Wpf.SQLConnectionDialog();
+            if (dlg.ShowDialog() ?? false)
+            {
+                var connectionstring = dlg.ConnectionString;
+                SourceConnectionString = connectionstring;
+                SourceDatabaseField.Text = connectionstring;
+            }
+        }
+
+        private void SelectTargetDatabaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new Wpf.SQLConnectionDialog();
+            if (dlg.ShowDialog() ?? false)
+            {
+                var connectionstring = dlg.ConnectionString;
+                TargetConnectionString = connectionstring;
+                TargetDatabaseField.Text = connectionstring;
+            }
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
